e4db1e4695adabaab592e18aa18967de243a5af7
Dipanjan Das <dipanjandas2758@gmail.com>
2025-12-03 13:20:14 +0530

    fix: Update API base URL to use production endpoint

--- Hospital-Management/Doctor/src/services/api.js
+++ Hospital-Management/Doctor/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1/doctor', 
+  baseURL: 'https://hms-backend-m5m4.onrender.com/api/v1/doctor', 
   withCredentials: true,
 })
 
