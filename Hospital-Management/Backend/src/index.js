535e7c33eced49cb827cf1663e2ce99689da5b6e
Dipanjan Das <dipanjandas2758@gmail.com>
2025-12-11 21:33:38 +0530

    refactor: simplify database connection handling in serverless function

--- Hospital-Management/Backend/src/index.js
+++ Hospital-Management/Backend/src/index.js
@@ -1,24 +1,16 @@
 import dotenv from "dotenv";
 import connectdb from "./db/index.js";
 import { app } from "./app.js";
-import serverlessExpress from "@vendia/serverless-express";
 
 dotenv.config();
 
-let server;
-
-async function initServer() {
-  if (!server) {
-    console.log("⏳ Connecting to MongoDB...");
-    await connectdb();             // Connect only once (NOT on every request)
-    console.log("✅ DB Connected!");
-
-    server = serverlessExpress({ app });
-  }
-  return server;
-}
+let isConnected = false;
 
 export default async function handler(req, res) {
-  const serverInstance = await initServer();
-  return serverInstance(req, res);
+    if (!isConnected) {
+        await connectdb();
+        isConnected = true;
+    }
+
+    return app(req, res);
 }
