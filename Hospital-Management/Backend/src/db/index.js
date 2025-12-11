e4a4ddaa9892b773231070ee9b40fdaaae8e3747
Dipanjan Das <dipanjandas2758@gmail.com>
2025-12-11 20:56:31 +0530

    fix: correct MongoDB connection string formatting

--- Hospital-Management/Backend/src/db/index.js
+++ Hospital-Management/Backend/src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 
 const connectdb = async () => {
     try {
-        const connectioninstance = await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`)
+        const connectioninstance = await mongoose.connect(`${process.env.MONGODB_URL}${process.env.DB_NAME}`)
         console.log("Mongodb CONNECTED !!");
         console.log("HOST:", connectioninstance.connection.host);
     } catch (error) {
