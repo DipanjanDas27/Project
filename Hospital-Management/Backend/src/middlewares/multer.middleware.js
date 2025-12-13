78c1f991fdccacc44549c254dc11abc9c894f454
Dipanjan Das <dipanjandas2758@gmail.com>
2025-12-13 14:09:59 +0530

    refactor: simplify multer middleware by removing disk storage and ensuring memory storage

--- Hospital-Management/Backend/src/middlewares/multer.middleware.js
+++ Hospital-Management/Backend/src/middlewares/multer.middleware.js
@@ -1,21 +1,8 @@
 import multer from "multer";
-import path from "path";
-import fs from "fs";
 
-const uploadDir = path.join(process.cwd(), "public", "temp");
-
-// ensure folder exists at runtime
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, uploadDir);
-  },
-  filename(req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`);
+export const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
   },
 });
-
-export const upload = multer({ storage });
